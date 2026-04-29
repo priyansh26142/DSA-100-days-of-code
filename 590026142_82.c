823e3a4d4aa420f8adfc819cbfe131007dad93e7
priyansh26142 <priyanshja345@gmail.com>
2026-04-29 05:56:53 +0000

    590026142_82.c

---
+++ 590026142_82.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int lower_bound(int arr[], int n, int x) {
+    int left = 0, right = n - 1;
+    int ans = n;  // default if not found
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] >= x) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int upper_bound(int arr[], int n, int x) {
+    int left = 0, right = n - 1;
+    int ans = n;  // default if not found
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] > x) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int x;
+    scanf("%d", &x);
+
+    int lb = lower_bound(arr, n, x);
+    int ub = upper_bound(arr, n, x);
+
+    printf("%d %d", lb, ub);
+
+    return 0;
+}
